6df20fa5a6a7fae0ecf895de2586aa01c286d051
Александр <hyperpositron@gmail.com>
2022-12-20 18:26:52 +0200

    Третья Задача ,переделал на один метод!

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,32 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();
-
     }
-        public static void task1() {
-            System.out.println("Задача 1");
+
+    public static void task1() {
+        //Задача 1
+        System.out.println("Задача 1");
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-            System.out.println(dog);
-            System.out.println(cat);
-            System.out.println(paper);
-    }
-        public static void task2() {
-            System.out.println("Задача 2");
-    var dog = 8.0;
-    var cat = 3.6;
-    var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        //Задача 2
+        System.out.println("Задача 2");
         dog = dog + 4; /* значение уже объявленной ранее переменной перезаменяется этой строчкой (ее значение увеличивается на 4) */
         cat = cat + 4;
         paper = paper + 4;
-            System.out.println(dog);
-            System.out.println(cat);
-            System.out.println(paper);
-        }
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        //Задача 3
+        System.out.println("Задача 3");
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
 }
