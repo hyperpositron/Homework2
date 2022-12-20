3fdb62992af7b2e5d3eeb82e746fb8b1f528ecbe
Александр <hyperpositron@gmail.com>
2022-12-20 17:48:21 +0200

    Вторая Задача

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
     }
         public static void task1() {
-        System.out.println("Задача 1");
+            System.out.println("Задача 1");
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+            System.out.println(dog);
+            System.out.println(cat);
+            System.out.println(paper);
     }
+        public static void task2() {
+            System.out.println("Задача 2");
+    var dog = 8.0;
+    var cat = 3.6;
+    var paper = 763789;
+        dog = dog + 4; /* значение уже объявленной ранее переменной перезаменяется этой строчкой (ее значение увеличивается на 4) */
+        cat = cat + 4;
+        paper = paper + 4;
+            System.out.println(dog);
+            System.out.println(cat);
+            System.out.println(paper);
+        }
 }
