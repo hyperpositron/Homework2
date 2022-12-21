188af4eb074bd437bcffa3c3c103ca0d409c4c4e
Александр <hyperpositron@gmail.com>
2022-12-21 17:27:49 +0200

    Восьмая Задача

--- src/Main.java
+++ src/Main.java
@@ -54,6 +54,28 @@ public class Main {
         System.out.println("Вес Второго боксера = " + boxer2 + " кг.");
         var totalWeight = (boxer1+boxer2);
         System.out.println("Общий все боксеров = " + totalWeight + " кг.");
-
+        //Задача 7
+        System.out.println("Задача 7");
+        //Вычислите разницу весов спортсменов, используйте 2 способа:
+        // Вычитание из большего веса меньшего.
+        var gapWeightBoxers = (boxer2 - boxer1);
+        System.out.println("Разница веса спортсменов = " + gapWeightBoxers + " кг.");
+        // С помощь функции остаток от деления.
+        var gapWeightBoxers2 = (boxer2) % boxer1;
+        System.out.println("Разница веса спортсменов остатком от деления = " + gapWeightBoxers2 + " кг.");
+        //Задача 8
+        System.out.println("Задача 8");
+        var employeeDedicatesToWork = (8); // сотрудник посвящает работе
+        System.out.println( employeeDedicatesToWork + " часов сотрудник посвящает работе.");
+        var openingHours = (640); // Часов работы
+        System.out.println( openingHours + " часов работы поделено между сотрудниками.");
+        var TotalEmployeesInTheCompany = (openingHours / employeeDedicatesToWork); //Всего работников в компании
+        System.out.println("«Всего работников в компании — " + TotalEmployeesInTheCompany + " человек».");
+        var additionalStaff = (94); // в компании работает на 94 человека больше
+        System.out.println("В компании работает на " + additionalStaff + " человека больше.");
+        var employee = (TotalEmployeesInTheCompany + additionalStaff);// всего сотрудников
+        var openingHours2 = (employee * employeeDedicatesToWork);// часов работы всего
+        System.out.println("«Если в компании работает " + employee + " человек, то всего " + openingHours2 + " часов " +
+        "работы может быть поделено между сотрудниками».");
     }
 }
